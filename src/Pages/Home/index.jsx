c3ad608fd644f405f691f58db0729ffbe8ea0778
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-11-24 03:41:39 +0000

    imported all files to home page, created components

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,27 @@
+import About from "../About";
+import Projects from "../Projects";
+import Experience from "../Experience";
+
+const Home = () => {
+
+    return (
+    <>
+      <header>
+{/* header content*/}
+      </header>
+
+      <main>
+<About />
+<Experience/>
+<Projects />
+      </main>
+
+      <footer>
+{/* footer content */}
+      </footer>
+    </>
+  );
+};
+
+
+export default Home;
